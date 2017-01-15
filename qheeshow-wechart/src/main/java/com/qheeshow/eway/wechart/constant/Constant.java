db2c49ab97155af852ab52192316a6edc1933670
lihuajun526 <lihuajun526@github.com>
2017-01-15 20:02:11 +0800

    'update'

---
+++ qheeshow-wechart/src/main/java/com/qheeshow/eway/wechart/constant/Constant.java
@@ -0,0 +1,14 @@
+package com.qheeshow.eway.wechart.constant;
+
+/**
+ * Created by lihuajun on 2016/9/11.
+ */
+public class Constant {
+
+    //微信access_token
+    public static String ACCESS_TOKEN;
+
+    public static String JSAPI_TICKET;
+
+
+}
--- service/src/main/java/com/qheeshow/eway/service/model/Goods.java
+++ service/src/main/java/com/qheeshow/eway/service/model/Goods.java
@@ -11,5 +11,45 @@ public class Goods extends BaseModel {
     private String desc;
     private Integer type;//商品类型
     private BigDecimal price;
+    private Integer status;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 }
